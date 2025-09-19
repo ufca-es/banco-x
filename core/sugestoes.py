0ec7917a17148a8d0836881df53e7c6295abc91a
Lux4477 <eliluciofilho@gmail.com>
2025-09-18 23:11:35 -0300

    Delete core directory Excluir excesso de arquivos passados

--- core/sugestoes.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Sugestoes:
-    @staticmethod
-    def adicionar_sugestao(pergunta):
-        with open(os.path.join(os.path.dirname(__file__), 'sugestoes.txt'), "a+", encoding="utf-8") as f:
-            f.write(f"Sugestão de pergunta: {pergunta}\n")
